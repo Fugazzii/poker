bcd2a9c0ad7459b7c22991e612b07ddfa2c0060a
Fugazzii <sichinavailia@gmail.com>
2023-10-04 23:19:27 +0400

    LICENSE

--- src/main.ts
+++ src/main.ts
@@ -6,39 +6,39 @@ const server = new Server();
 server.listen();
 
 // const socket0 = new WebSocket("ws://localhost:3001/");
-const socket = new WebSocket("ws://localhost:3001/");
+// const socket = new WebSocket("ws://localhost:3001/");
 
-socket.addEventListener("open", event => {
-    create({        
-        action: ACTION.NEW_PLAYER,
-        args: {
-            username: "ilia",
-            money: 1000
-        }
-    });
+// socket.addEventListener("open", event => {
+//     create({        
+//         action: ACTION.NEW_PLAYER,
+//         args: {
+//             username: "ilia",
+//             money: 1000
+//         }
+//     });
 
-    create({
-        action: ACTION.CREATE_BOARD,
-        args: {
-            admin: "ilia"
-        }
-    });
+//     create({
+//         action: ACTION.CREATE_BOARD,
+//         args: {
+//             admin: "ilia"
+//         }
+//     });
 
-    create({
-        action: ACTION.JOIN_BOARD,
-        args: {
-            playerUsername: "ilia",
-            boardId: "some_board_id"
-        }
-    })
-});
+//     create({
+//         action: ACTION.JOIN_BOARD,
+//         args: {
+//             playerUsername: "ilia",
+//             boardId: "some_board_id"
+//         }
+//     })
+// });
 
-socket.addEventListener("message", event => {
-    console.log(`Received from server: ${event.data}`);
-});
+// socket.addEventListener("message", event => {
+//     console.log(`Received from server: ${event.data}`);
+// });
 
-function create(socketBody: SocketBody) {
-    const request = JSON.stringify(socketBody);
+// function create(socketBody: SocketBody) {
+//     const request = JSON.stringify(socketBody);
 
-    socket.send(request);
-}
+//     socket.send(request);
+// }
